44ac05cf39e56a15710942cd683de924706e919b
RubenAnseeuw <ruben.anseeuw@student.kuleuven.be>
2021-11-12 15:12:08 +0100

    OLS

--- beginbestand.py
+++ beginbestand.py
@@ -10,10 +10,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 sp.init_printing(pretty_print=False)
 import random
-
+import statistics
 from numpy.random import *
 
-def f(x): return x**2
+def f(x): return 9*x-3
 
 def xsample(n, a=0, b=1):
     return np.linspace(a, b, n)
@@ -27,6 +27,18 @@ def ysample(x, f, e=1):
 
 
 
-xsample(5, a=0, b=1)
-print(ysample([1,2,3,4,5], f, e=1))
-# print(np.random.normal(2,3,5))
+x = xsample(5, 0,20)
+y = ysample(x, f, e=.3)
+
+def OLSVoorschriftBeter(x,y):
+    x_streep = statistics.mean(x)
+    y_streep = statistics.mean(y)
+    somteller = somnoemer = 0
+    for index in range(len(x)):
+        somteller += (x[index] - x_streep)*(y[index]+y_streep)
+        somnoemer += (x[index]-x_streep)**2
+    b = somteller/somnoemer
+    a = y_streep -b *x_streep
+    return a, b
+
+print(OLSVoorschriftBeter(x,y))
