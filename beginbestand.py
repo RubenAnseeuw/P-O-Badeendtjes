5be4995ae36502f9caabb960790259bc8b1b59d8
RubenAnseeuw <92731841+RubenAnseeuw@users.noreply.github.com>
2021-11-12 14:01:25 +0100

    Add files via upload

---
+++ beginbestand.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 13:55:58 2021
+
+@author: ansee
+"""
+
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+sp.init_printing(pretty_print=False)
+
+np.random
+
+np.xsample(5, a=0, b=1)
