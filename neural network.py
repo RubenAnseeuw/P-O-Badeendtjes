cf47d0513a1a7590f8d10c7028fa14747d8adba9
Athena1415 <dorien.roelandt@gmail.com>
2021-12-12 15:39:13 +0100

    vergelijking beter

--- neural network.py
+++ neural network.py
@@ -13,32 +13,32 @@ def LR(xx, yy):
     a, b = OLSVoorschriftBeter(xx, yy)
     xLR = np.linspace(0, 1, 100)
     yLR = [b * xje + a for xje in xLR]
-    plt.plot(xLR, yLR, label = "LR")
+    plt.plot(xLR, yLR, label = "Lineaire Regressie")
 
 def KNN(xx, yy):
     k = bepaalK_opt(20, xx, yy, f)
     xKNN = np.linspace(0, 1, 100)
     yKNN = [Knn_uiteindelijk(xje, xx, yy, k) for xje in xKNN]
-    plt.plot(xKNN, yKNN, label = "KNN, opt: %s" % k)
+    plt.plot(xKNN, yKNN, label = "KNN, Kopt: %s" % k)
 
 def pop():
     xPop = np.linspace(0, 1, 100)
     yPop = [f(xje) for xje in xPop]
-    plt.plot(xPop, yPop, label="pop")
+    plt.plot(xPop, yPop, label="Populatiefunctie")
 
-def NW():
-    xx = xsample(2000,0,1)
+def NN():
+    xx = xsample(2000, 0, 1)
     random.shuffle(xx)
     y = [f(xsje) for xsje in xx]
     X2 = [[1, xsje] for xsje in xx]
 
-    regr = MLPRegressor(activation="tanh", hidden_layer_sizes=20, random_state=3, max_iter=1000).fit(X2, y)
+    regr = MLPRegressor(activation = "tanh", hidden_layer_sizes = 20, random_state = 3, max_iter = 1000).fit(X2, y)
 
     xxx = np.linspace(0, 1, 100)
     XXX2 = [[1, xsje] for xsje in xxx]
     yyy = regr.predict(XXX2)
 
-    plt.plot(xxx, yyy, label = "NW")
+    plt.plot(xxx, yyy, label = "Neurale Netwerken")
 
 
 def main():
@@ -48,7 +48,7 @@ def main():
     LR(xx, yy)
     KNN(xx, yy)
     pop()
-    NW()
+    NN()
     plt.legend()
     plt.show()
 
